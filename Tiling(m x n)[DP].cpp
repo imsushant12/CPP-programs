099eba6deee73f41a02c0a7318c444eaceec64df
imsushant12 <sushantgaurav57@gmail.com>
2021-04-29 12:18:41 +0530

    first commit

--- Tiling(m x n)[DP].cpp
+++ Tiling(m x n)[DP].cpp
@@ -1,6 +1,38 @@
-
+#include <bits/stdc++.h>
+using namespace std;
 
 /*
 Time Complexity : O(n)
 Space Complexity : O(n)
 */
+
+int tiling(int n , int m)
+{
+    int dp[n+1];
+
+    for(int i=1 ; i<=n ; i++)
+    {
+        if(i<m)
+            dp[i] = 1;
+        else if(i == m)
+            dp[i] = 2;
+        else
+            dp[i] = dp[i-1] + dp[i-m];
+    }
+    return dp[n];
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter the length of the floor : ";
+    cin>>n;
+
+    int m;
+    cout<<"Enter the width of the floor : ";
+    cin>>m;
+
+    cout<<"Number of ways to tile the floor is : "<<tiling(n , m)<<endl;
+    return 0;
+}
+
