73a17c1efa99cea1af8c046837d147090101364b
imsushant12 <sushantgaurav57@gmail.com>
2021-06-01 21:59:13 +0530

    first commit

---
+++ Diagonal Traversal of Binary Tree.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	int data;
+	Node *left , *right;
+};
+
+Node* newNode(int data)
+{
+	Node* node = new Node;
+	node->data = data;
+	node->left = node->right = NULL;
+	return node;
+}
+
+void diagonalPrint(Node* root)
+{
+
+}
+
+int main()
+{
+	Node* root = newNode(8);
+	root->left = newNode(3);
+	root->right = newNode(10);
+	root->left->left = newNode(1);
+	root->left->right = newNode(6);
+	root->right->right = newNode(14);
+	root->right->right->left = newNode(13);
+	root->left->right->left = newNode(4);
+	root->left->right->right = newNode(7);
+
+	diagonalPrint(root);
+
+	return 0;
+}
+
