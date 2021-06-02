c0a28537d22a0ef1379bfc0dc017a8efa38d268e
imsushant12 <sushantgaurav57@gmail.com>
2021-06-02 14:59:08 +0530

    improved code

--- Diagonal Traversal of Binary Tree.cpp
+++ Diagonal Traversal of Binary Tree.cpp
@@ -3,37 +3,73 @@ using namespace std;
 
 struct Node
 {
-	int data;
-	Node *left , *right;
+    int data;
+    Node *left, *right;
 };
 
 Node* newNode(int data)
 {
-	Node* node = new Node;
-	node->data = data;
-	node->left = node->right = NULL;
-	return node;
+    Node* node = new Node;
+    node->data = data;
+    node->left = node->right = NULL;
+    return node;
 }
 
+vector <vector <int>> result;
 void diagonalPrint(Node* root)
 {
+    if(root == NULL)
+        return;
 
+    queue <Node*> q;
+    q.push(root);
+
+    while(!q.empty())
+    {
+        int size = q.size();
+        vector <int> answer;
+
+        while(size--)
+        {
+            Node* temp = q.front();
+            q.pop();
+
+            // traversing each component;
+            while(temp)
+            {
+                answer.push_back(temp->data);
+
+                if(temp->left)
+                    q.push(temp->left);
+
+                temp = temp->right;
+            }
+        }
+        result.push_back(answer);
+    }
 }
 
 int main()
 {
-	Node* root = newNode(8);
-	root->left = newNode(3);
-	root->right = newNode(10);
-	root->left->left = newNode(1);
-	root->left->right = newNode(6);
-	root->right->right = newNode(14);
-	root->right->right->left = newNode(13);
-	root->left->right->left = newNode(4);
-	root->left->right->right = newNode(7);
+    Node* root = newNode(8);
+    root->left = newNode(3);
+    root->right = newNode(10);
+    root->left->left = newNode(1);
+    root->left->right = newNode(6);
+    root->right->right = newNode(14);
+    root->right->right->left = newNode(13);
+    root->left->right->left = newNode(4);
+    root->left->right->right = newNode(7);
 
-	diagonalPrint(root);
+    diagonalPrint(root);
 
-	return 0;
+    for(int i=0 ; i<result.size() ; i++)
+    {
+        for(int j=0 ; j<result[i].size() ; j++)
+            cout<<result[i][j]<<"  ";
+        cout<<endl;
+    }
+
+    return 0;
 }
 
