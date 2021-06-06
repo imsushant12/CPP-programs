fcb2c1bce3742a99e817bae10d903d826e27a569
imsushant12 <sushantgaurav57@gmail.com>
2021-06-06 23:01:29 +0530

    first commit

---
+++ Fibonacci_DP--Top-down.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int fib[9];
+
+int Fibonacci(int n)
+{
+    if(n == 1)
+        return 1;
+    if(n == 2)
+        return 1;
+
+    // Can comment this line as well
+    if(fib[n] != 0)
+        return fib[n];
+
+    return (fib[n] = Fibonacci(n-1) + Fibonacci(n-2));
+}
+
+int main()
+{
+    cout<<"Fibonacci : "<<Fibonacci(9);
+
+    return 0;
+}
