0a3504a07eb8785ccda9f66710a83dc68ff7101a
imsushant12 <sushantgaurav57@gmail.com>
2021-06-06 21:58:38 +0530

    first commit

---
+++ Square_of_number.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    int tests=0,i=0;
+    cin>>tests;
+    int x[tests];
+
+    for(i = 0 ; i < tests ; i++)
+        cin>>x[i];
+ 
+    for(i = 0 ; i < tests ; i++)
+        cout<<x[i]*x[i]<<endl;
+
+    return 0;
+}
+
