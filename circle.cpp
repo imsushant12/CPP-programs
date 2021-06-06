f0bec4b0c688ef7fdf5dc8fc207ff930685594d9
imsushant12 <sushantgaurav57@gmail.com>
2021-06-06 22:16:49 +0530

    first commit

--- circle.cpp
+++ circle.cpp
@@ -1,16 +1,14 @@
-#include<graphics.h>
+#include <graphics.h>
 main()
 {
- int gd=5,gm;
- initgraph(&gd,&gm,"");
- setcolor(13);
- circle(200,100,50);
- setcolor(7);
- rectangle(40,90,80,150);
- setcolor(11);
- ellipse(250,240,0,360,50,70);
- getch();
- closegraph();
-
-
+    int gd = 5, gm;
+    initgraph(&gd, &gm, "");
+    setcolor(13);
+    circle(200, 100, 50);
+    setcolor(7);
+    rectangle(40, 90, 80, 150);
+    setcolor(11);
+    ellipse(250, 240, 0, 360, 50, 70);
+    getch();
+    closegraph();
 }
--- square.cpp
+++ square.cpp
@@ -1,10 +1,10 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
- cout<<"Enter number"<<endl;
- int x,y;
- cin>>x;
- y=x*x;
- cout<<"square of  "<<x<<" is "<<y;
+    cout << "Enter number" << endl;
+    int x, y;
+    cin >> x;
+    y = x * x;
+    cout << "square of  " << x << " is " << y;
 }
