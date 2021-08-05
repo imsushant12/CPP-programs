bd35a5b52d3341a0cf1a9542a7d5c6bef2b116a0
imsushant12 <sushantgaurav57@gmail.com>
2021-08-05 22:12:22 +0530

    first commit

--- Graph-Adjacency_List_using_Class.cpp
+++ Graph-Adjacency_List_using_Class.cpp
@@ -9,16 +9,16 @@ private:
     int V;
     //Array of lists
     //we don't know the size of array so using pointer to allocate memory dynamically.
-    list <int> *l;
+    list<int> *l;
 
 public:
     Graph(int V)
     {
         this->V = V;
-        l = new list <int> [V];                 //important, dynamically allocating memory
+        l = new list<int>[V]; //important, dynamically allocating memory
     }
 
-    void addEdges(int e1 , int e2)
+    void addEdges(int e1, int e2)
     {
         l[e1].push_back(e2);
         l[e2].push_back(e1);
@@ -26,12 +26,12 @@ public:
 
     void display(int V)
     {
-        cout<<"List representation is : "<<endl;
-        for(int i=0 ; i<V ; i++)
+        cout << "List representation is : " << endl;
+        for (int i = 0; i < V; i++)
         {
-            cout << i << "--->";
-            list <int> :: iterator it;
-            for(it = l[i].begin(); it != l[i].end(); ++it)
+            cout << i << " --> ";
+            list<int>::iterator it;
+            for (it = l[i].begin(); it != l[i].end(); it++)
                 cout << *it << " ";
             cout << endl;
         }
@@ -42,28 +42,22 @@ int main()
 {
     Graph g(4);
 
-    g.addEdges(0 , 1);
-    g.addEdges(0 , 2);
-    g.addEdges(1 , 2);
-    g.addEdges(2 , 3);
+    g.addEdges(0, 1);
+    g.addEdges(0, 2);
+    g.addEdges(1, 2);
+    g.addEdges(2, 3);
 
     g.display(4);
 
     return 0;
-/*
-Graph:
-       1
-      / \
-     /   \
-    0-----2
-          |
-          |
+    /*
+    Graph:
+             1
+           / \
+         /   \
+        0----2
+            |
+           |
           3
-*/
+    */
 }
-
-
-
-
-
-
