faa687d139d319e31952f48c9f178ad72b8f28bd
Sushant Gaurav <sushantgaurav57@gmail.com>
2021-10-20 17:24:27 +0530

    first commit

--- Bucket_or_Bin_Sort.cpp
+++ Bucket_or_Bin_Sort.cpp
@@ -1,60 +1,64 @@
 #include <bits/stdc++.h>
-#include <vector>
 using namespace std;
 
 void bin_sort(float a[], int n)
 {
-    int i,j;
+    int i, j;
     //creating buckets
-    vector <float> b[n];
+    vector<float> b[n];
 
     //putting array elements into their respective buckets.
-    for(i=0 ; i<n ; i++)
+    for (i = 0; i < n; i++)
     {
-        int bi = n * a[i];            //index in bucket (important step)
+        int bi = n * a[i]; //index in bucket (important step)
         b[bi].push_back(a[i]);
     }
 
     //sorting the individual buckets
-    for (i=0 ; i<n ; i++)
-        sort(b[i].begin() , b[i].end());
+    for (i = 0; i < n; i++)
+        sort(b[i].begin(), b[i].end());
 
     //concatenating all the buckets into the array
     int index = 0;
-    for(i=0 ; i<n ; i++)            //traversing buckets
-        for(j=0 ; j<b[i].size() ; j++)      //traversing individual bucket
+    for (i = 0; i < n; i++)               //traversing buckets
+        for (j = 0; j < b[i].size(); j++) //traversing individual bucket
             a[index++] = b[i][j];
 }
 
-
 int main()
 {
-    int n,i;
+    int n, i;
 
-    cout<<"Enter number of elements of array : ";
-    cin>>n;
+    cout << "Enter number of elements of array : ";
+    cin >> n;
 
     float a[n];
 
-    cout<<"Enter elements of array : "<<endl;
-    for(i=0 ; i<n ; i++)
-        cin>>a[i];
+    cout << "Enter elements of array : " << endl;
+    for (i = 0; i < n; i++)
+        cin >> a[i];
 
-    bin_sort(a , n);
+    bin_sort(a, n);
 
-    cout<<"\n\nSorted array is : "<<endl;
-    for(i=0 ; i<n ; i++)
-        cout<<a[i]<<"  ";
+    cout << "\n\nSorted array is : " << endl;
+    for (i = 0; i < n; i++)
+        cout << a[i] << "  ";
 
-        /*
-    return 0;
-    float arr[]
-        = { 0.897, 0.565, 0.656, 0.1234, 0.665, 0.3434 };
-    int n = sizeof(arr) / sizeof(arr[0]);
-    bucketSort(arr, n);
-
-    cout << "Sorted array is \n";
-    for (int i = 0; i < n; i++)
-        cout << arr[i] << " ";*/
     return 0;
 }
+
+/*
+Sample Input-Output:
+-------------------
+Enter number of elements of array : 5
+Enter elements of array : 
+0.897
+0.565
+0.656
+0.1234
+0.665
+
+
+Sorted array is :
+0.1234  0.565  0.656  0.665  0.897
+*/
