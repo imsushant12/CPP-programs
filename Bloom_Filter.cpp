9e2f4749bec9399974a0e19f2c2738cb28a4f837
Sushant Gaurav <sushantgaurav57@gmail.com>
2021-10-20 17:24:19 +0530

    first commit

--- Bloom_Filter.cpp
+++ Bloom_Filter.cpp
@@ -1,23 +1,20 @@
 #include <bits/stdc++.h>
-#include <vector>
 using namespace std;
 
-//Simple Bloom Filter implementation with
-//k=2(hash functions) and m=5(bloom filter's size)
+// Simple Bloom Filter implementation with
+// k = 2(hash functions) and m=5(bloom filter's size)
 
 class BloomFilter
 {
 private:
-
     int SIZE;
     int bloom[5];
 
 public:
-
     BloomFilter()
     {
         SIZE = 5;
-        for(int i=0 ; i<SIZE ; i++)
+        for (int i = 0; i < SIZE; i++)
             bloom[i] = 0;
     }
 
@@ -28,7 +25,7 @@ public:
 
     int hash2(int data)
     {
-        return (((2*data) + 3) % SIZE);
+        return (((2 * data) + 3) % SIZE);
     }
 
     void insertElements(int data)
@@ -45,7 +42,7 @@ public:
         int index1 = hash1(data);
         int index2 = hash2(data);
 
-        if((bloom[index1] == 0) || (bloom[index2] == 0))
+        if ((bloom[index1] == 0) || (bloom[index2] == 0))
             return true;
         else
             return false;
@@ -53,63 +50,62 @@ public:
 
     void displayFilter()
     {
-        cout<<"\nBloom Filter formed is : "<<endl;
-        cout<<"INDEX   BIT"<<endl;
-        for(int i=0 ; i<SIZE ; i++)
-            cout<<i<<"  --->  "<<bloom[i]<<endl;
+        cout << "\nBloom Filter formed is : " << endl;
+        cout << "INDEX   BIT" << endl;
+        for (int i = 0; i < SIZE; i++)
+            cout << i << "  --->  " << bloom[i] << endl;
     }
-
 };
 
 int main()
 {
     BloomFilter b;
-    int choice,data;
+    int choice, data;
 
-    while(1)
+    while (1)
     {
-        cout<<"\n1.Insert \t 2.Check Element \t 3.Display Bloom Filter \t 4.EXIT"<<endl;
-        cout<<"Enter choice : ";
-        cin>>choice;
+        cout << "\n1.Insert \t 2.Check Element \t 3.Display Bloom Filter \t 4.EXIT" << endl;
+        cout << "Enter choice : ";
+        cin >> choice;
 
-        switch(choice)
+        switch (choice)
         {
         case 1:
-            {
-                cout<<"\nEnter element : ";
-                cin>>data;
-                b.insertElements(data);
+        {
+            cout << "\nEnter element : ";
+            cin >> data;
+            b.insertElements(data);
 
-                break;
-            }
+            break;
+        }
 
         case 2:
-            {
-                cout<<"\nEnter element : ";
-                cin>>data;
+        {
+            cout << "\nEnter element : ";
+            cin >> data;
 
-                bool result = b.checkElement(data);
+            bool result = b.checkElement(data);
 
-                if(result == true)
-                    cout<<"Element is not present"<<endl;
-                else
-                    cout<<"Element may be present (FALSE POSITIVE)"<<endl;
+            if (result == true)
+                cout << "Element is not present" << endl;
+            else
+                cout << "Element may be present (FALSE POSITIVE)" << endl;
 
-                break;
-            }
+            break;
+        }
 
         case 3:
-            {
-                b.displayFilter();
-                break;
-            }
+        {
+            b.displayFilter();
+            break;
+        }
 
         case 4:
             exit(0);
             break;
 
         default:
-            cout<<"\nINVALID CHOICE\n";
+            cout << "\nINVALID CHOICE\n";
         }
     }
     return 0;
