1ca118ca4f53c2899804b506d4b37e513aef51fc
Sushant Gaurav <sushantgaurav57@gmail.com>
2021-10-20 17:25:40 +0530

    first commit

---
+++ Project_Euler-Multiples_of_3_or_5.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int sum = 0;
+
+    for (int i = 0; i < 1000; i++)
+    {
+        if (i % 3 == 0 || i % 5 == 0)
+        {
+            sum = sum + i;
+        }
+    }
+
+    cout << sum << endl;
+
+    return 0;
+}
