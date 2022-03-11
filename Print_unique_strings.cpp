4bfd4d67147b9f5cae241fd5408533d3a1f4ff81
Sushant Gaurav <sushantgaurav57@gmail.com>
2022-03-11 23:25:09 +0530

    first commit

---
+++ Print_unique_strings.cpp
@@ -0,0 +1,36 @@
+/*
+Problem Statement:
+------------------
+Given N strings, print unique strings in lexicographical order with their frequency.
+N <= 10^5
+|S| <= 100
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    map<string, int> m;
+
+    int n;
+    cin >> n;
+
+    while (n--)
+    {
+        string s;
+        cin >> s;
+
+        if (m.find(s) != m.end())
+            m[s]++;
+
+        else
+            m.insert({s, 1});
+    }
+
+    for (auto i : m)
+        cout << i.first << "  ->  " << i.second << endl;
+    
+
+    return 0;
+}
